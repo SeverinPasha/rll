2fea327441ba045767705d02606f15f5ddf22061
Severin Pasha <shadowthet@gmail.com>
2017-11-27 22:05:13 +0300

    fix1

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
+var box = document.querySelector(".box");
+
 function roll() {
     function random(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
     var valueOfRandom = random(0, 100);
-
-
+        
     switch (true) {
 
         case valueOfRandom < 5:
-            alert("black");
+            // alert("black");  
+            box.className = "box1";
+
             break;
 
         case valueOfRandom > 6 && valueOfRandom < 15:
-            alert("yellow");
+            // alert("yellow");
+            box.className = "box2";
             break;
 
         case valueOfRandom > 16 && valueOfRandom < 35:
-            alert("red");
+            // alert("red");
+            box.className = "box3";
             break;
 
         case valueOfRandom > 36 && valueOfRandom < 60:
-            alert("blue");
+            // alert("blue");
+            box.className = "box4";
             break;
 
         case valueOfRandom > 61:
-            alert("green");
+            // alert("green");
+            box.className = "box5";
             break;
-    };
+                };
 
 }
